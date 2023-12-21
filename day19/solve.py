d02eccb0a3ace82dffbb69008d9d350fedec1d38
Jonathan Fraser <jonathanfras@gmail.com>
2023-12-21 00:50:15 -0800

    day19 part2

--- day19/solve.py
+++ day19/solve.py
@@ -3,41 +3,101 @@ import math
 import numpy as np
 from scipy.linalg import null_space
 import scipy.sparse as sp
-import queue 
+import queue
+import copy
 from typing import List
+import pprint
 from dataclasses import dataclass,field
 folder = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(folder,"input.txt")
 
+keys = ['x','m','a','s']
 def total_part(part: dict):
     return sum(part.values())
 
-def run_rule(rule,part):
+def in_options(opts,part):
+    for o in opts:
+        is_in = True 
+        for (k,value) in part.items():
+            if value > o[k][1]:
+                is_in = False 
+                break 
+
+            if value < o[k][0]:
+                is_in = False
+                break
+
+        if is_in:
+            return True
+    return False
+
+def opt_calc(opt):
+    o = 1 
+    for k in keys:
+        (srt,end) = opt[k]
+        o *= (end-srt+1)
+    return o 
+        
+def run_part_match(rule,part_match):
+    outputs = []
+    remainder = copy.deepcopy(part_match)
     for (match,tgt) in rule:
+        if remainder is None:
+            continue 
+
         if match is None:
-            return tgt 
-        (key,dir,val) = match
+            outputs.append((tgt,remainder))
+            remainder = None 
+            continue 
+
+        (key,dir,val) = match 
+
+        keep = None 
+        rej = None 
+
+        (srt,end) = remainder[key]
+
         if dir == '>':
-            if part[key] > val:
-                return tgt 
+            keep = (val+1,end)
+            rej = (srt,val)
+
         if dir == '<':
-            if part[key] < val:
-                return tgt
-        
-    raise RuntimeError("no tail case")
+            keep = (srt,val-1)
+            rej = (val,end)
 
-def run_part(rules,part):
-    current_rule = "in"
-    while True:
-        rule = rules[current_rule]
-        prev_rule = current_rule
-        current_rule = run_rule(rule,part)
+        (ksrt,kend) = keep
+        if ksrt <= kend and ksrt >= srt and kend <= end:
+            new_out = copy.deepcopy(remainder)
+            new_out[key]=keep
+            outputs.append((tgt,new_out))
 
-        if current_rule == "A":
-            return total_part(part)
+        (rsrt,rend) = rej 
+        if rsrt <= rend and rsrt >= srt and rend <= end:
+            remainder[key]=rej
+        else:
+            remainder = None
+
+    return outputs 
+
         
-        if current_rule == "R":
-            return 0 
+def run_part_matches(rules):
+    run_set = [("in",{'x':(1,4000),'m':(1,4000),'a':(1,4000),'s':(1,4000),"seen":[]})]
+    accepted = []
+    while len(run_set):
+        (tgt,options) = run_set[0]
+        options['seen'].append(tgt)
+        run_set = run_set[1:]
+        rule = rules[tgt]
+        results = run_part_match(rule,options)
+        for (t,o) in results:
+            if t == "A":
+                accepted.append(o)
+                continue 
+            if t == "R":
+                continue 
+            run_set.append((t,o))
+
+    return accepted
 
 def construct_rule(str):
     end = str.find(":")
@@ -47,7 +107,8 @@ def construct_rule(str):
     tgt = str[end+1:]
 
     return ((key,dir,val),tgt)
-
+    
+    
 
 def solve(): 
     with open(input_file,'r') as f:
@@ -77,7 +138,12 @@ def solve():
             rule_step.append((None, step_strings[-1]))
             rules[name] =rule_step
 
-        print(sum([run_part(rules,p) for p in parts]))
+        opts =  run_part_matches(rules)
+        accepted_parts = [p for p in parts if in_options(opts,p)]
+        print(sum([total_part(p) for p in accepted_parts]))
+        print(sum([opt_calc(o) for o in opts]))
+
+
 
     
         
